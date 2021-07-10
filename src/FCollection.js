aa36636896622bb5cc66b32f2f2ec08ea7194269
Caitlin Raby <caitlinraby@gmail.com>
2021-07-10 16:58:36 -0500

    commit tree 093c5eb38eaf2f606ed4c12de348c070d9ac31b7

---
+++ src/FCollection.js
@@ -0,0 +1,15 @@
+import ForageablesCard from "./ForageablesCard";
+
+function ForageablesCollection ({forageables}) {
+
+    return(
+        <div>
+
+            {forageables.map(f => <ForageablesCard key={f.id} forageables={f} image={f.image} name={f.name} scientific={f.scientific} recipe={f.recipe}/>)}
+            
+        </div>
+    )
+
+}
+
+export default ForageablesCollection;
---
+++ src/ForageablesContainer.js
@@ -0,0 +1,25 @@
+import { useEffect , useState } from "react";
+import ForageablesCollection from "./FCollection.js";
+
+function ForageablesContainer() {
+    const [forageables, setForageables] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3004/forageables")
+        .then (r => r.json())
+        .then (data => setForageables(data))
+
+    },[]);
+    
+  
+      
+    return (
+        <div>
+
+            <ForageablesCollection forageables={forageables}/>
+        
+        </div>
+    )
+}
+
+export default ForageablesContainer;
