70f3072327e09ef68ef92780ea324ff4a3ed73ae
Caitlin Raby <caitlinraby@gmail.com>
2021-07-10 16:59:40 -0500

    adding some initial static components

---
+++ src/ForageablesCard.js
@@ -0,0 +1,13 @@
+function ForageablesCard({name, id, image, recipe, scientific}) {
+    
+    return(
+        <div>
+            <h3>{name}</h3>
+            <h4>{scientific}</h4>
+            <p>{recipe}</p>
+            <img src={image} alt={name}></img>
+        </div>
+    )
+}
+
+export default ForageablesCard;
